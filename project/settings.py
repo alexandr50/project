d2e32d586d7dfcea79139fc0c6097de0e8aa9e88
alexandr <dmitrymaslov2016@mai.ru>
2023-09-06 23:35:39 +0300

    add app bot

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    "phonenumber_field",
+    'users',
+    'bot',
 ]
 
 MIDDLEWARE = [
