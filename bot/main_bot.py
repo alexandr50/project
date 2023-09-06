593f3618e4b7e2e5bb4643e151bca7e5fd6dc4dc
alexandr <dmitrymaslov2016@mai.ru>
2023-09-06 23:48:13 +0300

    add buttons for bot

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,4 +1,4 @@
-from telebot.async_telebot import AsyncTeleBot
+from telebot.async_telebot import AsyncTeleBot, types
 
 from django.conf import settings
 
@@ -7,7 +7,12 @@ bot = AsyncTeleBot(settings.TOKEN_BOT, parse_mode='HTML')
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    await bot.send_message(message.chat.id, 'Пользовательское соглашение')
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=False, one_time_keyboard=True)
+    stop = types.KeyboardButton(text='отмена')
+    agree = types.KeyboardButton(text='принять')
+    keyboard.add(stop)
+    keyboard.add(agree)
+    await bot.send_message(message.chat.id, 'Пользовательское соглашение', reply_markup=keyboard)
 
 
 
